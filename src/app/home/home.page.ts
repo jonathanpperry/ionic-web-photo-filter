b35a04044d63ec9a669aff46c2b038c1310419ef
Jonathan Perry <jpper@umich.edu>
2020-05-07 13:04:18 +0900

    Commit of partially completed tutorial

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,30 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
+})
+export class HomePage {
+  image: any = "./assets/img/test.jpg";
+
+  filterOptions = [
+    { name: "Normal", value: "" },
+    { name: "Sepia", value: "sepia" },
+    { name: "Blue Monotone", value: "blue_monotone" },
+    { name: "Violent Tomato", value: "violent_tomato" },
+    { name: "Grey", value: "grey" },
+    { name: "Brightness", value: "brightness" },
+    { name: "Saturation", value: "saturation" },
+  ];
+
+  slideOpts = {
+    slidesPerView: 3.5,
+    spaceBetween: 5,
+    slidesOffsetBefore: 20,
+    freeMode: true,
+  };
+  constructor() {}
+
+  filter(index) {}
+}
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
+
+import { defineCustomElements } from "@ionic/pwa-elements/loader";
+
+// After the platform has been bootstrapped
+defineCustomElements(window);
