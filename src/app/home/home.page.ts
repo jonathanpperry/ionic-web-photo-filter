6755cb54c3135706a977ae5e034f0a8ea7fb6e63
Jonathan Perry <jpper@umich.edu>
2020-05-08 00:12:08 +0900

    Finished tutorial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Plugins, CameraResultType, CameraSource } from "@capacitor/core";
 
 @Component({
   selector: "app-home",
@@ -6,14 +7,15 @@ import { Component } from "@angular/core";
   styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-  image = "assets/img/test.jpg";
-
+  image = "";
+  selectedFilter = "";
+  result: HTMLElement;
   filterOptions = [
     { name: "Normal", value: "" },
     { name: "Sepia", value: "sepia" },
     { name: "Blue Monotone", value: "blue_monotone" },
     { name: "Violent Tomato", value: "violent_tomato" },
-    { name: "Grey", value: "grey" },
+    { name: "Grey", value: "greyscale" },
     { name: "Brightness", value: "brightness" },
     { name: "Saturation", value: "saturation" },
   ];
@@ -26,5 +28,48 @@ export class HomePage {
   };
   constructor() {}
 
-  filter(index) {}
+  imageLoaded(e) {
+    console.log("loaded: ", e);
+    this.result = e.detail.result;
+  }
+
+  filter(index) {
+    this.selectedFilter = this.filterOptions[index].value;
+  }
+
+  async selectImage() {
+    const image = await Plugins.Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera,
+    });
+    this.image = image.dataUrl;
+  }
+
+  saveImage() {
+    let base64 = "";
+    if (this.selectedFilter) {
+      // Use the original image
+      base64 = this.image;
+    } else {
+      let canvas = this.result as HTMLCanvasElement;
+      // export as dataUrl or Blob!
+      base64 = canvas.toDataURL("image/jpeg", 1.0);
+    }
+
+    // Do something with the result
+    this.downloadBase64File(base64);
+  }
+
+  downloadBase64File(contentBase64) {
+    const linkSource = `${contentBase64}`;
+    const downloadLink = document.createElement("a");
+    document.body.appendChild(downloadLink);
+
+    downloadLink.href = linkSource;
+    downloadLink.target = "_self";
+    downloadLink.download = "test.png";
+    downloadLink.click();
+  }
 }
